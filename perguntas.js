1de46a7e04a16c3c48ca9756af58ea64c597a0f3
agentphpp <paulohenriquepereiralontra@gmail.com>
2024-11-09 21:58:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,55 @@
+criaCartao(
+    'História',
+    'Qual foi o motivo principal que levou ao fim do sistema feudal na Europa?',
+    'A Peste Negra e a crise econômica e social'
+)
+
+criaCartao(
+    'História',
+    'Qual tratado encerrou oficialmente a Primeira Guerra Mundial?',
+    'O Tratado de Versalhes'
+
+)
+
+criaCartao(
+    'História',
+    'Qual civilização antiga foi a primeira a desenvolver um sistema de escrita e onde ela se originou?',
+    ' A civilização suméria, na Mesopotâmia'
+)
+criaCartao(
+    'Instrumento',
+    'Quantas cordas possuem um violino?',
+    '4 cordas - Sol, Re, La, Mi'
+)
+
+criaCartao(
+    'Programação',
+    'Qual linguagem é utilizada na Unity?',
+    'é utilizado C#'
+
+)
+
+criaCartao(
+    'Programação',
+    'Como imprimir uma informação na tela usando C#?',
+    'Console.WriteLine();'
+)
+criaCartao(
+    'Programação',
+    'O que é o conceito de "herança" em programação orientada a objetos ?',
+    'Herança  permite que uma classe reutilize atributos e métodos de outra, facilitando o reaproveitamento de código.'
+)
+
+criaCartao(
+    'Programação',
+    'Qual linguagem é recomendada para iniciantes que possuem o idioma português?',
+    'Linguagem Portugol'
+
+)
+
+criaCartao(
+    'Musica',
+    'Do que um pentagrama(pauta) é composto(a)?',
+    '5 linhas e 4 espaços;'
+)
+
